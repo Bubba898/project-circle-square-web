261ba7efd6957a34ee5de92bd1fd72468cb0a91d
Yannick Wiggers <yannick.wiggers@kolibrigames.com>
2025-01-25 17:52:29 +0100

    Fixes sending messages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,17 +8,24 @@
   let server_url: string = "project-circle-square-server.onrender.com"
   let simulate_unity: boolean = false
   let use_wss: boolean = false
-  let message: string = ""
+  let message_text: string = ""
+  $: messages = [""]
 
   function on_game_start(message: Message) {
     connection_state = "game_started"
   }
 
-  function on_message(message: Message) {
-    alert(`GameStarted: ${message}`)
+  function on_message_json(message: Message) {
+    messages.push(JSON.stringify(message))
     if(message.event == "GameStart") {
       on_game_start(message)
     }
+    messages = messages
+  }
+
+  function on_message(message: string) {
+    messages.push(message)
+    messages = messages
   }
 
   function connect(event: MouseEvent): void {
@@ -27,22 +34,22 @@
     ws = new WebSocket(protocol + "://" + server_url + "/" + client_type + "/" + session_id);
     connection_state = "waiting_for_other"
     ws.onmessage = function (event) {
-      // exisiting example code
-      let messages = document.getElementById('messages')
-      let message2 = document.createElement('li')
-      let content = document.createTextNode(event.data)
-      message2.appendChild(content)
-      messages?.appendChild(message2)
-
-      // real implementation
-      const message: Message = JSON.parse(event.data)
-      on_message(message)
+      console.log(event)
+      try {
+        const message: Message = JSON.parse(event.data)
+        on_message_json(message)
+      } catch {
+        const message = event.data
+        on_message(message)
+      }
     };
   }
 
-  function sendMessage(event: SubmitEvent) {
-    ws?.send(message)
-    message = ''
+  function sendMessage(event: MouseEvent) {
+    console.log(message_text)
+    //messages.push(message_text)
+    ws?.send(message_text)
+    message_text = ""
   }
 </script>
 
@@ -50,7 +57,7 @@
 <h1>Game State: {connection_state} {simulate_unity}</h1>
 <hr/>
 {#if connection_state === 'not_connected'}
-<form on:submit|preventDefault={sendMessage}>
+<form>
   <p><label>Session ID: <input type="number" bind:value={session_id}/></label></p>
   <p><label>Server URL: <input type="text" bind:value={server_url}/></label></p>
   <label>Simulate Unity Client: <input type="checkbox" bind:checked={simulate_unity}/></label>
@@ -71,8 +78,11 @@
     Your Session ID: {session_id}
   </p>
   <hr/>
-  <label>Message: <input type="text" id="messageText" autocomplete="off"/></label>
-  <button class="btn btn-primary">Send</button>
+  <label>Message: <input type="text" bind:value={message_text}/></label>
+  <button class="btn btn-primary" on:click|preventDefault={sendMessage}>Send</button>
+  {#each messages as message}
+    <li>{message}</li>
+  {/each}
 {/if}
 <ul id='messages'>
 </ul>
